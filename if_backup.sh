992392ba8dbc9c0c5b41f2c858c9043b9bd1c47b
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-12-23 10:34:41 +0200

    Update if_backup.sh

--- if_backup.sh
+++ if_backup.sh
@@ -4,9 +4,9 @@
 mkdir -p /root/ironfish/ironfish-cli/backup/
 echo $IRONFISH_NODENAME
 cd /root/ironfish/ironfish-cli/backup/ 
-ironfish accounts:export $IRONFISH_NODENAME /root/ironfish/ironfish-cli/backup/$IRONFISH_NODENAME.txt
-ironfish accounts:export  $IRONFISH_WALLET $IRONFISH_WALLET.json
+ironfish accounts:export $IRONFISH_NODENAME /root/ironfish/ironfish-cli/backup/$IRONFISH_NODENAME.txt && sleep 5
+ironfish accounts:export  $IRONFISH_WALLET $IRONFISH_WALLET.json && sleep 5
 cd /root/ironfish/ironfish-cli/
 cp $IRONFISH_WALLET.json /root/ironfish/ironfish-cli/backup/ 
 cd /root/ironfish/ironfish-cli/backup/
-cat  $HOME/.ironfish/config.json | sudo tee -a nodename_graffiti.txt 
+cat  $HOME/.ironfish/config.json |  tee -a nodename_graffiti.txt 
