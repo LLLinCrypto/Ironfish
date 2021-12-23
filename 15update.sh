eccd9bb81a41c51c6ddb9cc23855e60be6d8c3f0
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-12-23 12:00:59 +0200

    Create 15update.sh

---
+++ 15update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. $HOME/.bashrc
+. $HOME/.bash_profile
+mkdir -p ~/if_wallet/backup/
+echo $IRONFISH_NODENAME
+INN=$IRONFISH_NODENAME
+export INN=$INN >> $HOME/.bash_profile&source ~/.bash_profile
+
+ironfish accounts:export $IRONFISH_WALLET ~/if_wallet/backup/$IRONFISH_WALLET.json
+echo " wallet exported"
+
+cd ~/if_wallet/backup/
+cat  $HOME/.ironfish/config.json |  tee -a nodename_graffiti.txt 
+echo $INN
+cat  ~/ironfish_keys/$IRONFISH_WALLET.json
+echo "Success!!!"
