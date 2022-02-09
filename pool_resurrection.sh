819e9a09c2fdd7e936be7ff61a6eca1d5fa69cc2
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-09 22:22:48 +0200

    Create pool_resurrection.sh

---
+++ pool_resurrection.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#systemctl stop ironfish
+service ironfishd-miner stop
+service ironfishd stop
+cp /etc/systemd/system/ironfishd-miner_backup.service /etc/systemd/system/ironfishd-miner.service
+
+
+systemctl daemon-reload
+sudo systemctl restart systemd-journald
+sudo systemctl restart ironfishd ironfishd-miner
