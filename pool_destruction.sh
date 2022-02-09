3e3d59234c43b898df32c47ca04817e94e3b9041
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-09 22:19:31 +0200

    Create pool_destruction.sh

---
+++ pool_destruction.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#systemctl stop ironfish
+service ironfishd-miner stop
+service ironfishd stop
+cp /etc/systemd/system/ironfishd-miner.service /etc/systemd/system/ironfishd-miner_backup.service
+rm -rf /etc/systemd/system/ironfishd-miner.service
+
+
+echo "[Unit]
+Description=IronFish Miner
+After=network-online.target
+[Service]
+User=root
+ExecStart=/usr/bin/yarn --cwd /root/ironfish/ironfish-cli/ start miners:start -t -1
+Restart=always
+RuntimeMaxSec=36000
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target" > $HOME/ironfishd-miner.service
+mv $HOME/ironfishd-miner.service /etc/systemd/system/
+systemctl daemon-reload
+sudo systemctl restart systemd-journald
+sudo systemctl restart ironfishd ironfishd-miner
