9923116f256ed48a6db0847c6c9c85da8f89e029
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-19 15:47:44 +0200

    Create clustersatellite.sh

---
+++ clustersatellite.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#systemctl stop ironfish
+service ironfishd-miner stop
+service ironfishd stop
+cp /etc/systemd/system/ironfishd.service /etc/systemd/system/ironfishd1.service
+cp /etc/systemd/system/ironfishd-miner.service /etc/systemd/system/ironfishd-miner1.service
+
+rm -rf /etc/systemd/system/ironfishd-miner.service
+rm -rf /etc/systemd/system/ironfishd.service
+
+echo "[Unit]
+Description=IronFish Node
+After=network-online.target
+[Service]
+User=root
+ExecStart=/usr/bin/yarn --cwd /root/ironfish/ironfish-cli/ start miners:start -t -1
+Restart=always
+RuntimeMaxSec=36000
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target" > $HOME/ironfishd-miner.service
+mv $HOME/ironfishd-miner.service /etc/systemd/system/
+
+echo "[Unit]
+Description=IronFish Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start start 
+Restart=always
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+" > $HOME/ironfishd.service
+echo "[Unit]
+Description=IronFish Miner
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start miners:start -t -1 --rpc.tcp --rpc.tcp.host=144.126.154.61  --rpc.tcp.port=8001
+Restart=always
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+" > $HOME/ironfishd-miner.service
+sudo mv $HOME/ironfishd.service /etc/systemd/system
+sudo mv $HOME/ironfishd-miner.service /etc/systemd/system
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+
+systemctl daemon-reload
+sudo systemctl restart systemd-journald
+sudo systemctl restart ironfishd ironfishd-miner
