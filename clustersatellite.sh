fbf7c06bd7b6cabe5f8e0963c276b65cc90cc2b8
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-03-10 12:34:03 +0200

    Update clustersatellite.sh

--- clustersatellite.sh
+++ clustersatellite.sh
@@ -39,7 +39,7 @@ Description=IronFish Miner
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start miners:start -t -1 --rpc.tcp --rpc.tcp.host=207.244.237.158  --rpc.tcp.port=8001
+ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start miners:start -t -1 --rpc.tcp --rpc.tcp.host=144.126.136.215  --rpc.tcp.port=8001
 Restart=always
 RestartSec=10
 LimitNOFILE=10000
